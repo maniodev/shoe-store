af2c324d156d91d164c3d6fd5298c1de674f4c2a
Hermann <maniodev@Hermanns-MBP.fritz.box>
2020-12-27 03:39:07 +0100

    Update inventorie_channel js to update the view

--- inventory_monitor/app/javascript/channels/inventories_channel.js
+++ inventory_monitor/app/javascript/channels/inventories_channel.js
@@ -1,12 +1,46 @@
 import consumer from "./consumer"
 import Chart from "chart.js"
+
 consumer.subscriptions.create("InventoriesChannel", {
   connected() {},
   disconnected() {},
   received(data) {
     if(data.type === "chart") { refreshChart(data.data); }
+    if(data.type === "sales") { refreshSales(data.data); }
+    if(data.type === "notifications") { refreshNotifications(data.data); }
+    if(data.type === "select_inputs") {
+      refreshSelectInputs(data.data.shoe_models, "shoe");
+      refreshSelectInputs(data.data.store_names, "store");
+    }
   }
 });
+
+function refreshSales(data){
+  var elem = document.getElementById("sales")
+  var result = ""
+  data.forEach(element => {
+    var badge = "";
+    var arrow = "";
+    if(element.trend === "up") {
+      badge = "success";
+      arrow = "<i class='fa fa-arrow-up ml-1'></i>"
+    } else if (element.trend === "down") {
+      badge = "danger";
+      arrow = "<i class='fa fa-arrow-down ml-1'></i>"
+    } else {
+      badge = "primary"
+    }
+    result += `
+    <a class="list-group-item list-group-item-action waves-effect">${element.shoe_model}
+    <span class="badge badge-${badge} badge-pill pull-right">${element.percentage}%
+      ${arrow}
+    </span>
+    </a>
+    `
+  });
+  elem.innerHTML = result;
+}
+
 function refreshChart(data) {
   if( window.myBar!==undefined) {
     window.myBar.destroy();
@@ -62,6 +96,29 @@ function refreshChart(data) {
     });
 }
 
+function refreshNotifications(data) {
+  var elem = document.getElementById("notification")
+  var result = ""
+  data.forEach(element => {
+    result += `
+    <li class="list-group-item">${element}</li>
+    `
+  });
+  elem.innerHTML = result;
+}
+
+function refreshSelectInputs(data, id) {
+  var sel = document.getElementById(id);
+  var selected_option = sel.options[sel.selectedIndex].text;
+  var list = "";
+  
+  list += `<option value="">All</option>`;
+  for(var i = 0; i < data.length; i++) {
+    var selected = data[i] === selected_option ? "selected" : "";
+    list += `<option ${selected} value='${data[i]}'>${data[i]}</option>`;
+  }
+  sel.innerHTML = list;
+}
 
 
 
--- inventory_monitor/spec/controllers/admin/dashboard_controller_spec.rb
+++ inventory_monitor/spec/controllers/admin/dashboard_controller_spec.rb
@@ -3,6 +3,16 @@
 require "rails_helper"
 
 RSpec.describe Admin::DashboardController, type: :controller do
+  before { allow(RefreshChart).to receive(:call) }
+
+  shared_examples "calls refreshchart to refresh the chart" do
+    it do
+      expect(RefreshChart).to receive(:call).with(params: params)
+
+      post :refresh, params: params
+    end
+  end
+
   describe "#index" do
     let!(:shoes) { create_list(:shoe, 2) }
     let!(:stores) { create_list(:store, 2) }
@@ -15,5 +25,12 @@ RSpec.describe Admin::DashboardController, type: :controller do
       expect(assigns(:store_names)).to eq(stores.map(&:name))
     end
 
+    it_behaves_like "calls refreshchart to refresh the chart"
+  end
+
+  describe "#chart" do
+    let(:params) { { store: "ALDO Crossgates Mall", shoe_model: "GRELIDIEN" } }
+
+    it_behaves_like "calls refreshchart to refresh the chart"
   end
 end
